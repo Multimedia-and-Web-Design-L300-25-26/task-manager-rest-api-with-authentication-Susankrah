5378e29105a4e1c6cad638b9a34fd824e0ad9dcb
Susankrah <susankrah2@gmail.com>
2026-03-05 14:37:06 +0000

    authentication

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 
 // Create User schema
 // Fields:
@@ -9,10 +9,38 @@ import mongoose from "mongoose";
 // - createdAt (default Date.now)
 
 
-
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true ,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+     trim: true,
+  },
+  password: {
+    type : String,
+    required: true,
+    minlength: 6
+  },
+  }, {
+  timestamps: true,
+
   // Students implement
 });
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
